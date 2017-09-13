56b1a863491aa34dfb6bec5efa299ff8ad399f5e
dtoropin <dennis.tor@gmail.com>
2017-09-13 13:44:49 +0300

    add object view

--- js/battleship.js
+++ js/battleship.js
@@ -1,1 +1,18 @@
 // View
+var view = {
+    showMessage: function (msg) {
+        document.getElementById("messageArea").innerHTML = msg;
+    },
+
+    showMiss: function (location) {
+        document.getElementById(location).setAttribute("class", "miss");
+    },
+
+    showHit: function (location) {
+        document.getElementById(location).setAttribute("class", "hit");
+    }
+};
+
+// view.showMessage("Seabattle!");
+// view.showMiss("05");
+// view.showHit("34");
